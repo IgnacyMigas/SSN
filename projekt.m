9622fefb26c453a37eb366afa313ec0bdb995286
Daria <dariasiemieniuk@gmail.com>
2019-06-25 21:41:33 +0200

    test commit

--- projekt.m
+++ projekt.m
@@ -4,3 +4,4 @@ clear;
 [Xu_imgs, Yu_labels] = readMNIST('train-images.idx3-ubyte', 'train-labels.idx1-ubyte', 60000, 0);
 [Xt_imgs, Yt_labels] = readMNIST('t10k-images.idx3-ubyte', 't10k-labels.idx1-ubyte', 10000, 0);
 
+
