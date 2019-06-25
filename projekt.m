2a3410ff7d092c2357430a67b759d4d819d0902c
Daria <dariasiemieniuk@gmail.com>
2019-06-25 23:23:51 +0200

    Only more comments

--- projekt.m
+++ projekt.m
@@ -1,14 +1,17 @@
 clc;
 clear;
 
+%%% training data
 [Xu_imgs, Yu_labels] = readMNIST('train-images.idx3-ubyte', 'train-labels.idx1-ubyte', 60000, 0);
+%%% testing data
 [Xt_imgs, Yt_labels] = readMNIST('t10k-images.idx3-ubyte', 't10k-labels.idx1-ubyte', 10000, 0);
 
-% prepare data for network creation
-% T = ...;
+%%% prepare data for network creation
+%%% T = ...;
 
+% xx = Xu_imgs(:,:,1) % checking
 
-% create a Hopfield recurrent function
-% net = newhop(T);
-newhop();
+%%% create a Hopfield recurrent network
+%%% net = newhop(T);
+% net = newhop();
 
